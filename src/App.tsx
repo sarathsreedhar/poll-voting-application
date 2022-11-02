e57f549d44b89307708ac554fb8712359a7e8721
sarath <sarath.knd@gmail.com>
2022-11-02 13:31:57 +0100

    update and delete local file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { Routes, Route } from 'react-router-dom'
+import ErrorPage from './components/errorPage'
 import Layout from './components/Layout'
-import { QuestionList } from './features/poll/QuestionList'
+import { QuestionDetailsPage } from './features/poll/QuestionDetailsPage'
+import { QuestionListPage } from './features/poll/QuestionList/QuestionListPage'
 
 function App() {
   return (
     <div className="App">
-      <Layout>
-        <QuestionList />
-      </Layout>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <QuestionListPage />
+            </Layout>
+          }
+        />
+        <Route path="questions/:id" element={<QuestionDetailsPage />} />
+
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </div>
   )
 }
